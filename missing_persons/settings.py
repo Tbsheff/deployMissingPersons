7bc75a0586bb7a61eae7376593e9f5366567e796
Tyler Sheffield <tylersheffield427@gmail.com>
2023-04-11 20:07:02 -0600

    commit

--- missing_persons/settings.py
+++ missing_persons/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://missingpersons-production.up.railway.app/']
+ALLOWED_HOSTS = [
+    '*', 'https://deploymissingpersons-production.up.railway.app/']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
